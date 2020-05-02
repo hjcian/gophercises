7d36a64f0d04180300255b6729a05a05c29b8c4e
hjcian <chutarojian@gmail.com>
2020-05-02 15:45:59 +0800

    ✨ Feat: Add maps example

---
+++ tdd/maps/dictionary.go
@@ -0,0 +1,54 @@
+package dictionary
+
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	v, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return v, nil
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = def
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = newDefinition
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
